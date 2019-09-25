f1077af0338b3ebafe7eda5568a04fc0ef323a1d
Tiara <tbell1@aggies.ncat.edu>
2019-09-25 16:49:19 -0400

    add set up for left and right arrays

--- CapMetro/src/capmetro/DivideAndConquer.java
+++ CapMetro/src/capmetro/DivideAndConquer.java
@@ -12,27 +12,61 @@ package capmetro;
 public class DivideAndConquer {
 
     public static int findHighestPassengerDensity(int[] values) {
-        int a = 0;
-        int z = values.length - 1;
-        if (values.length == 1) {
-            return values[1];
+        
+        int n = values.length;
+        int maxDensity = 0;
+        int currMax = 0;
+       
+        if (n == 1) {
+            return values[0];
         }
-        int mid = (a + z) / 2;
+        int leftSize = n/2;
+        int rightSize = n - leftSize;
 
-        int[] B = new int[100];
-        int[] C = new int[100];
-        for (int i = 0; i <= mid; i++) {
-            B[i] = values[a];
+        int[] B = new int[leftSize];
+        int[] C = new int[rightSize];
+        
+        //Initializing left side array
+        for (int i = 0; i < leftSize; i++) {
+            B[i] = values[i];
         }
-
-        for (int j = mid + 1; j <= values.length; j++) {
-            C[j] = values[j];
+        //Initializing right side array
+        for (int i = leftSize; i <= n; i++) {
+            C[i - leftSize] = values[i];
         }
+        
+        for (int j = 0; j < B.length; j++) {
+            if (currMax >= maxDensity) {
+                maxDensity = currMax;
+            }
+            if (B[j] >= 0) {
+                currMax += B[j];
+            } else if (currMax > B[j]) {
+                currMax += B[j];
+            } else if (currMax < B[j]) {
+                currMax = 0;
+            }
+        }
+        
+        for (int j = 0; j < C.length; j++) {
+            if (currMax >= maxDensity) {
+                maxDensity = currMax;
+            }
+            if (C[j] >= 0) {
+                currMax += C[j];
+            } else if (currMax > C[j]) {
+                currMax += C[j];
+            } else if (currMax < C[j]) {
+                currMax = 0;
+            }
+        }
+        
 
         int rB = findHighestPassengerDensity(B);
         int rC = findHighestPassengerDensity(C);
-        int rA = findCrossPassengerDensity(B, C);
-        return (max(rA, rB, rC));
+        //int rA = findCrossPassengerDensity(B, C);
+        //return (max(rA, rB, rC));
+        return (max(rB, rC));
     }
 
     public static int findCrossPassengerDensity(int[] B, int[] C) {
