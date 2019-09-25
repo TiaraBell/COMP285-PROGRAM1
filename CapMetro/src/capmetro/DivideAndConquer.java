f82a54e8f57db325eae453a57e21dc1470a118ca
Tiara <tbell1@aggies.ncat.edu>
2019-09-24 21:35:35 -0400

    add findCPD and max method skeletons

--- CapMetro/src/capmetro/DivideAndConquer.java
+++ CapMetro/src/capmetro/DivideAndConquer.java
@@ -10,10 +10,39 @@ package capmetro;
  * @author tiara
  */
 public class DivideAndConquer {
-    public static int findHighestPassengerDensity (int[] values) {
-    	//  You need to write this method. 
-    	
-    	return -1;
+
+    public static int findHighestPassengerDensity(int[] values) {
+        int a = 0;
+        int z = values.length - 1;
+        if (values.length == 1) {
+            return values[1];
+        }
+        int mid = (a + z) / 2;
+
+        int[] B = new int[100];
+        int[] C = new int[100];
+        for (int i = 0; i <= mid; i++) {
+            B[i] = values[a];
+        }
+        
+        for (int j = mid + 1; j <= values.length; j++) {
+            C[j] = values[j];
+        }
+          
+        
+
+        int rB = findHighestPassengerDensity(B);
+        int rC = findHighestPassengerDensity(C);
+        int rA = findCrossPassengerDensity(B, C);
+        return (max(rA, rB, rC));
+    }
+    
+    public static int findCrossPassengerDensity(int[] B, int[] C) {
+        return -1;
+    }
+    
+    public static int max(int[] values, int[] B, int[] C) {
+        return -1;
     }
     
 }
--- CapMetro/src/capmetro/UpdatedDriver.java
+++ CapMetro/src/capmetro/UpdatedDriver.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 package capmetro;
 
 import capmetro.DivideAndConquer;
@@ -12,17 +5,11 @@ import capmetro.Iterative;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Random;
-/**
- *
- * @author tiara
- */
-public class Driver {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        ArrayList<Integer> values = new ArrayList<>();
+public class UpdatedDriver {
+	public static void main(String[] args) {
+		
+	    ArrayList<Integer> values = new ArrayList<Integer>();
 
 	    /*Test0: An array with one positve element
 	     *   expect: 10
@@ -68,63 +55,6 @@ public class Driver {
 	    values.add(9);
 	    values.add(-4);
 	    runAlgs(values);
-            
-            /*Test5: An array with one element 
-	     *   expect: 0
-	     */
-	    values.clear();
-	    values.add(0);
-	    runAlgs(values);
-            
-            /*Test6: An array with multiple elements 
-	     *   expect: -1
-	     */
-	    values.clear();
-	    values.add(-4);
-            values.add(-7);
-            values.add(-2);
-            values.add(-1);
-            values.add(-11);
-	    runAlgs(values);
-            
-            /*Test7: An array with multiple elements
-	     *   expect: 0
-	     */
-	    values.clear();
-	    values.add(-5);
-            values.add(-8);
-            values.add(0);
-            values.add(-3);
-            values.add(-2);
-            values.add(0);
-	    runAlgs(values);
-            
-            /*Test8: An array with multiple elements
-	     *   expect: 2
-	     */
-	    values.clear();
-	    values.add(0);
-            values.add(2);
-            values.add(-1);
-            values.add(-5);
-            values.add(-9);
-            values.add(-10);
-            values.add(2);
-            values.add(-2);
-	    runAlgs(values);
-            
-            /*Test9: An array with multiple elements
-	     *   expect: 7?
-	     */
-	    values.clear();
-            values.add(1);
-	    values.add(1);
-            values.add(1);
-            values.add(1);
-            values.add(1);
-            values.add(1);
-            values.add(1);
-	    runAlgs(values);
 	}
 	
 	static void runAlgs(ArrayList<Integer> values) {
@@ -146,7 +76,6 @@ public class Driver {
 		System.out.println("           and the answer is " + highestPassengerDensity);
 		System.out.println("");
 	}
-
 	//turn an ArrayList<Integer> into an int[]
     public static int[] createValueArray(ArrayList<Integer> list){
 		int n = list.size();
@@ -154,6 +83,18 @@ public class Driver {
 	    for(int i = 0; i<n; i++){
 	    	values[i] = list.get(i).intValue();
 	    }
-		return values;
+		   return values;
     }
+    
+	// randomly generate a sequence of integers for blocks
+	public static ArrayList<Integer> generateInput(int n){
+	    ArrayList<Integer> list = new ArrayList<Integer>(n);
+	    Random random = new Random();
+	    for(int i = 0; i<n; i++){
+	    	//randomly make a negative or positive number
+	    	list.add(random.nextInt(65536)-32768);
+	    }
+	    java.util.Collections.shuffle(list);
+	    return list;
+	}
 }
